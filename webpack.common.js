fd9707209ea1c0cfdaa49e2ca48849805570ba8c
hazall <denizlihazal@gmail.com>
2021-02-19 17:39:51 +0300

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+	entry: __dirname + '/src/scripts/app.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	resolve: {
+		modules: [
+			'src',
+			'node_modules',
+		],
+	},
+	module: {
+		rules: [
+			{
+				test: /.(mp4|webm)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						outputPath: 'video',
+					},
+				},
+			},
+			{
+				test: /.(woff|woff2)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						outputPath: 'fonts',
+					},
+				},
+			},
+			{
+				test: /.(png|svg|jpe?g|gif|webp)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						outputPath: 'images',
+					},
+				},
+			},
+		],
+	},
+}
